8259a2dfe7c2a0ec0babf9cd8acb5dc1c49940db
matt.l <limchan1@naver.com>
2020-12-21 10:32:43 +0900

    version3

--- src/main/java/com/matt/intellij/Main2.java
+++ src/main/java/com/matt/intellij/Main2.java
@@ -4,5 +4,6 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("bbb");
         System.out.println("version2");
+        System.out.println("version3");
     }
 }
